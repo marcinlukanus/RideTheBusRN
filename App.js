01b5363d6ced8454bd7accfaaebfca94e86328f6
unknown <mlukanus@gmail.com>
2020-05-27 22:36:34 -0500

    Adds buttons but needs additional styling for placement

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import {
+  Button,
   SafeAreaView,
   StyleSheet,
   ScrollView,
@@ -22,15 +23,25 @@ const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <View style={styles.root}>
+      <View style={styles.container}>
         <Header />
+		<View style={{ width: 200 }}>
+			<Button
+				title="Draw Cards"
+				color="#5cb85c"
+			/>
+			<Button
+				title="Rules"
+				color="#495057"
+			/>
+		</View>
       </View>
 	</>
   );
 };
 
 const styles = StyleSheet.create({
-	root: {
+	container: {
 		flex: 1,
 		backgroundColor: '#22303f'
 	}
