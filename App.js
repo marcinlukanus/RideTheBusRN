84a5ec2d2f41d8a9694ac343f1edf308ad294755
unknown <mlukanus@gmail.com>
2020-05-31 12:21:49 -0500

    Replaces Button elements with TouchableOpacity elements and adds styling

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
   ScrollView,
   View,
   Text,
+  TouchableOpacity,
   StatusBar,
 } from 'react-native';
 
@@ -24,18 +25,16 @@ const App: () => React$Node = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <View style={styles.container}>
-        <Header />
-		<View style={{ width: 200 }}>
-			<Button
-				title="Draw Cards"
-				color="#5cb85c"
-			/>
-			<Button
-				title="Rules"
-				color="#495057"
-			/>
+		<Header />
+		<View style={styles.buttons}>
+			<TouchableOpacity style={styles.drawCardsButton}>
+				<Text style={styles.buttonText}>Draw Cards</Text>
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.rulesButton}>
+				<Text style={styles.buttonText}>Rules</Text>
+			</TouchableOpacity>
 		</View>
-      </View>
+	  </View>
 	</>
   );
 };
@@ -43,7 +42,27 @@ const App: () => React$Node = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#22303f'
+		backgroundColor: '#22303f',
+	},
+	buttons: {
+		flexDirection: 'row',
+		width: '100%',
+		height: '25%',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	drawCardsButton: {
+		backgroundColor: '#5cb85c',
+		padding: 8,
+		margin: 10
+	},
+	rulesButton: {
+		backgroundColor: '#495057',
+		padding: 8,
+		margin: 10
+	},
+	buttonText: {
+		color: 'white'
 	}
 });
 
