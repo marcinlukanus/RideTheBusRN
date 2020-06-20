617c5fed1572d607bdb64bb3eac57b6ac645ade3
unknown <mlukanus@gmail.com>
2020-06-20 17:37:42 -0500

    Adds stubbed methods used for starting game and flipping cards

--- helpers.js
+++ helpers.js
@@ -11,10 +11,40 @@ import {
   StatusBar,
   Alert,
 } from 'react-native';
+import axios from 'axios';
 
 const helpers = {
+    flipCard: function(cardNum) {
+      Alert.alert('TODO', 'Lorem ipsum');
+      // switch(cardNum) {
+      //     case 0:
+      //         this.cardOneFlipped = true
+      //         break
+      //     case 1:
+      //         this.cardTwoFlipped = true
+      //         break
+      //     case 2:
+      //         this.cardThreeFlipped = true
+      //         break
+      //     case 3:
+      //         this.cardFourFlipped = true
+      //         break
+      //     default:
+      //         break
+      // }
+    },
     rules: function() {
         Alert.alert('TODO', 'Lorem ipsum');
+    },
+    startGame: function() {
+      axios
+        .get('https://deckofcardsapi.com/api/deck/new/draw/?count=4')
+        .then(response => {
+          Alert.alert('TODO', 'Lorem ipsum');
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
 }
 
