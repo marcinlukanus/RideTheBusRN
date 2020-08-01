939393eaa119933d4168fd9b0ecce7c765a35744
unknown <mlukanus@gmail.com>
2020-08-01 09:06:59 -0500

    Adds in rules text for Rules button

--- helpers.js
+++ helpers.js
@@ -34,13 +34,19 @@ const helpers = {
       // }
     },
     rules: function() {
-        Alert.alert('TODO', 'Lorem ipsum');
+        Alert.alert(
+            'Rules', 
+            '1. Pick the color. [RED or BLACK]\n\n' +
+            '2. Pick the number. [HIGHER or LOWER or SAME]\n\n' + 
+            '3. Pick the range. [INSIDE or OUTSIDE or SAME]\n\n' + 
+            '4. Pick the suit.\n    [HEARTS or DIAMONDS or SPADES or CLUBS]'
+        );
     },
     startGame: function() {
       axios
         .get('https://deckofcardsapi.com/api/deck/new/draw/?count=4')
         .then(response => {
-          Alert.alert('TODO', 'Lorem ipsum');
+          Alert.alert('TODO', response.data);
         })
         .catch(error => {
           console.log(error)
